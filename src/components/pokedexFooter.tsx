a1b9ccdee6100c74f69867fc7697ed4ab0ac3034
Nilton Cosme <niltoncosta@pecege.com>
2023-04-05 00:30:45 -0300

    adicionado conteudo do footer

--- src/components/pokedexFooter.tsx
+++ src/components/pokedexFooter.tsx
@@ -1,3 +1,4 @@
+import { SvgIcon } from "./SvgIcon"
 import "./style/pokedexFooter.css"
 
 interface IPokedexFooter {
@@ -10,8 +11,19 @@ export const PokedexFooter = (props : IPokedexFooter) => {
         bottom: "0"
     }
 
+    const gitHubIconStyle : React.CSSProperties = {
+        width: "1rem",
+        height: "1rem"
+    }
+
     return (
         <div className="footer" style={props.isGenZero ? footerStyle : {} as React.CSSProperties}>
+            <div className="footerChild">
+                <h4>
+                    <SvgIcon wrapperStyle={gitHubIconStyle} iconPath="/github-mark-white.svg"></SvgIcon>
+                    <a className="linkColor" href="https://github.com/ncjr1"> Check out my GitHub</a> 
+                </h4>
+            </div>
         </div>
     )
 }
